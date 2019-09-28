1db6284470a9f9eb0dccb8796b02ec4b136e83c4
abhisek <abhisek1651990@gmail.com>
2019-09-28 20:36:55 +0100

    removed pylinting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,17 +1,15 @@
 from django.test import TestCase
-
 from app.calc import add, sub
 
 
 class ClassTest(TestCase):
 
-
     def test_addnum(self):
 
         '''Test for addition of numbers'''
-        self.assertEqual(add(3,4),7)
+        self.assertEqual(add(3, 4), 7)
 
     def test_subtructnum(self):
 
         '''Subtract two numbers'''
-        self.assertEqual(sub(4,3),1)
+        self.assertEqual(sub(4, 3), 1)
--- app/core/tests/test_modules.py
+++ app/core/tests/test_modules.py
@@ -1,47 +1,36 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
 
+class ModelTests(TestCase):
 
     def test_create_user_with_email(self):
 
         email = 'abcdxyz@gmail.com'
         password = 'abcd123'
-        user = get_user_model().objects.create_user(
-        email = email,
-        password = password
-        )
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_create_user_with_normalized_email(self):
 
         email = 'abcdxyz@GMAIL.COM'
-        user = get_user_model().objects.create_user(
-        email = email,
-        password = 'test123'
-        )
+        user = get_user_model().objects.create_user(email=email,
+                                                    password='test123')
         self.assertEqual(user.email, email.lower())
 
     def test_create_superuser(self):
 
         email = 'abcdxyzsup@GMAIL.COM'
-        user = get_user_model().objects.create_superuser(
-        email = email,
-        password = 'test123'
-        )
+        user = get_user_model().objects.create_superuser(email=email,
+                                                         password='test123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
-
-
 
     def test_create_user_with_valid_email(self):
 
         with self.assertRaises(ValueError):
-            user = get_user_model().objects.create_user(email = None,
-            password = 'test123'
-            )
+            get_user_model().objects.create_user(email=None,
+                                                 password='test123')
